a6db5ce62be795d2955d52ab01de41ce06df9418
Mrzhang <mrzhangrb@outlook.com>
2017-08-22 16:20:04 +0800

    fix issure that Twist display problem

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,8 +7,13 @@
 
             @foreach($twists as $twist)
                 <div class="panel panel-default">
-                    <div class="panel-heading">Twist</div>
-                    <a href="{{ $twist->path() }}"><div class="panel-body">{{ $twist->body }}</div></a>
+                    <div class="panel-heading"> {{ $twist->user->name }} said : </div>
+                    <a href="{{ $twist->path() }}">
+                    <div class="panel-body">{{ $twist->body }}</div></a>
+                          <div class="panel-footer">
+                              {{ $twist->updated_at->diffForHumans() }}
+                          </div>
+                    
                 </div>
             @endforeach
             </div>   
